32311121bbe37dbacff1a049f21674332da74670
Du <mariaegsp@gmail.com>
2024-02-15 11:58:02 -0300

    selecionando os dados da tabela clientes com python

--- clientes.py
+++ clientes.py
@@ -14,3 +14,22 @@ conexao = mc.connect(
 )
 
 print(conexao)
+
+# para se movimentar dentro da estrutura de banco de dados e retornar dos dados necessários, iremos criar um cursor
+
+cursor = conexao.cursor()
+
+# Executar um comando usando o cursor
+# cursor.execute("create database OLA")
+
+cursor.execute("insert into clientes(nome_cliente,email,telefone)values('Amanda','mands@gmail.com','(54)99886-3425)")
+
+# Vamos selecionar todos dados da tabela clietes
+cursor.execute("Select * from banco.cliente")
+
+print(cursor)
+
+for c in cursor:
+    print(f"Id do Cliente: {c[0]}")
+    print(f"nome do Cliente: {c[1]}")
+    print(f"E-mail: {c[2]}")
